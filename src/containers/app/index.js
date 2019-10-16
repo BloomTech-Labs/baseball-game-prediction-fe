af34ab620ccefe605882253c23e8d9fe11761f9f
Anthony Johnson <beatsbyseptember@gmail.com>
2019-10-16 11:31:23 -0400

    added Nav using Material-UI

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import Home from '../home'
 import About from '../about'
+import Nav from '../nav/index'
 
 const App = () => (
   <div>
+    <Nav />
     <header>
       <Link to="/">Home</Link>
       <Link to="/about-us">About</Link>
