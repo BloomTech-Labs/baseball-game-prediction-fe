7e422ac5c5cdf041079394382bc2f99415126525
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-10-22 15:26:44 -0600

    Generated divisional lists to branch schedule off

--- src/containers/schedules/index.js
+++ src/containers/schedules/index.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { axiosWithAuthMSF } from '../../utils/axiosWithAuthMSF';
+import SimpleList from './SimpleList';
 
 export default function SchedulesPage() {
-    const [teams, setTeams] = useState([])
-    const [schedule, setSchedule] = useState([])
+    const [teams, setTeams] = useState([]);
 
     useEffect(() => {
         // axiosWithAuthMSF()
@@ -16,24 +16,47 @@ export default function SchedulesPage() {
         //     console.log(err);
         // })
         axios.get('https://bgp-be-staging.herokuapp.com/api/teams')
-        .then(teams => {
-            setTeams(teams.data);
+        .then(teaminfo => {
+            setTeams(teaminfo.data);
         })
         .catch(err => {
             console.log(err);
         })
     }, [])
 
+    // Divisional filters
+
+    const nlWTeams = () => {return teams.filter(team => {
+    return team.league === "NL" && team.division === "West";})};
+
+    const nlETeams = () => {return teams.filter(team => {
+    return team.league === "NL" && team.division === "East";})};
+
+    const nlCTeams = () => {return teams.filter(team => {
+    return team.league === "NL" && team.division === "Central";})};
+
+    const alWTeams = () => {return teams.filter(team => {
+    return team.league === "AL" && team.division === "West";})};
+
+    const alETeams = () => {return teams.filter(team => {
+    return team.league === "AL" && team.division === "East";})};
+
+    const alCTeams = () => {return teams.filter(team => {
+    return team.league === "AL" && team.division === "Central";})};
+
+    if (teams.length > 1){
     return (
-        <div>
-            {teams ? teams.map(team => {
-                return (
-                    <div>
-                        <h1>{team.team_name}</h1>
-                        <h2>{team.league}{" "}{team.division}</h2>
-                    </div>
-                )
-            }) : () => {return null}}
+        <div className="list-container">
+            <SimpleList division={nlWTeams()}/>
+            <SimpleList division={nlETeams()}/>
+            <SimpleList division={nlCTeams()}/>
+            <SimpleList division={alWTeams()}/>
+            <SimpleList division={alETeams()}/>
+            <SimpleList division={alCTeams()}/>
         </div>
-    );
+    );} else {
+        return (
+            <div/>
+        )
+    }
 }
