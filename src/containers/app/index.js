d5af66f515cf33881886bbc5c94328f2344d726a
Michael Mangus <mjmangus16@yahoo.com>
2019-10-22 11:32:08 -0400

    cleaning up files

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,27 +1,19 @@
 import React from "react";
-// import { Route, Link } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 
 import Nav from "../nav/index";
-import Schedules from '../schedules';
-
+import Schedules from "../schedules";
+import Home from "../home";
+import About from "../about";
 
 const App = () => (
   <div>
     <Nav />
-<<<<<<< HEAD
-=======
-    <header>
-      <Link to="/">Home</Link>
-      <Link to="/about-us">About</Link>
-      <Link to="/schedules">Schedules</Link>
-    </header>
-
     <main>
       <Route exact path="/" component={Home} />
       <Route exact path="/about-us" component={About} />
-      <Route exact path="/schedules" component={Schedules}/>
+      <Route exact path="/schedules" component={Schedules} />
     </main>
->>>>>>> 5560859a2c95a0327b1e9c8351f9be97fa182ae8
   </div>
 );
 
--- src/containers/login/index.js
+++ src/containers/login/index.js
@@ -1,78 +1,78 @@
 // Render Prop
-import React, { Component } from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
-import {connect} from 'react-redux'
-import {login} from '../../actions/index'
-import { Link } from 'react-router-dom'
-
+import React, { Component } from "react";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { connect } from "react-redux";
+import { login } from "../../Redux/actions/index";
+import { Link } from "react-router-dom";
 
 class Login extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-        username:'',
-        password:''
-        }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-       }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-      
-        handleChange(e) { 
-            
-           this.setState( {value: e.target.value} );
-        }
-       
-  
-       handleSubmit(e) {
-            e.preventDefault();
-         this.props.login(this.state.username, this.state.password);
-       }
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.login(this.state.username, this.state.password);
+  }
 
   render(props) {
-      return (
-        <div className="LoginForm">
-        <Form error={this.props.loginError ? true : false} size='large' onSubmit={this.handleSubmit}>
-            <FormGroup>
-                <Label for="userName">userName</Label>
-                <Input 
-                    type="text" 
-                    name="username" 
-                    id="username" 
-                    placeholder="User Name" 
-                    onChange={this.handleChange} 
-                    value={this.state.username}  
-                    />
-            </FormGroup>
-            <FormGroup>
-                <Label for="userPassword">Password</Label>
-                <Input 
-                    type="password" 
-                    name="password" 
-                    id="password" 
-                    placeholder="Password" 
-                    onChange={this.handleChange} 
-                    value={this.state.password}  
-                    />
-            </FormGroup>
-            <Button value="submit">Submit</Button>
-            <br/>
+    return (
+      <div className="LoginForm">
+        <Form
+          error={this.props.loginError ? true : false}
+          size="large"
+          onSubmit={this.handleSubmit}
+        >
+          <FormGroup>
+            <Label for="userName">userName</Label>
+            <Input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="User Name"
+              onChange={this.handleChange}
+              value={this.state.username}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="userPassword">Password</Label>
+            <Input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              onChange={this.handleChange}
+              value={this.state.password}
+            />
+          </FormGroup>
+          <Button value="submit">Submit</Button>
+          <br />
 
-          
-            <span>
-              New to us? <Link to="/register">Register</Link>
-            </span>
-            
-        
+          <span>
+            New to us? <Link to="/register">Register</Link>
+          </span>
         </Form>
       </div>
-      )
+    );
   }
 }
 
 const mapStateToProps = state => {
-    return {
-      loginError: state.loginError
-    }
-  }
-  export default connect(mapStateToProps, {login})(Login)
+  return {
+    loginError: state.loginError
+  };
+};
+export default connect(
+  mapStateToProps,
+  { login }
+)(Login);
