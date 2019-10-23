14c0fcb7ddd847d256731982e570b34f99491541
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-10-23 16:06:41 -0600

    Added Team Schedule view

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Route, Link } from "react-router-dom";
 import Profile from "../profile/index.js"
 import Nav from "../nav/index";
-import Schedules from "../schedules";
+import Schedules from "../schedules/DivisionListView";
+import TeamSchedule from "../schedules/TeamScheduleView";
 import Home from "../home";
 import About from "../about";
 
@@ -13,6 +14,7 @@ const App = () => (
       <Route exact path="/" component={Home} />
       <Route exact path="/about-us" component={About} />
       <Route exact path="/schedules" component={Schedules} />
+      <Route exact path="/schedules/:team_id" component={TeamSchedule} />
       <Route exact path="/profile" component={Profile} />
     </main>
   </div>
