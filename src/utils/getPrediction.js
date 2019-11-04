68839597afeeb580b37680d29886f046226f2f50
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-11-04 16:45:53 -0700

    Added top half of the data needed for the prediction model

--- src/utils/getPrediction.js
+++ src/utils/getPrediction.js
@@ -4,10 +4,7 @@ import { axiosWithAuth } from "./axiosAuth"
 
 export function getPrediction(date, away, home) {
 
-  console.log(date, away, home);
-
   const getLineup = async () => {
-    console.log(date, away, home);
     const gameLineup = await axiosWithAuthMSF().get(
       `https://api.mysportsfeeds.com/v2.1/pull/mlb/2019-regular/games/${date}-${away}-${home}/lineup.json`
     );
@@ -26,8 +23,6 @@ export function getPrediction(date, away, home) {
         teams: teamArray
       };
 
-      console.log(gameInfo);
-
       return gameInfo
     
     } else {
@@ -37,72 +32,78 @@ export function getPrediction(date, away, home) {
 
   getLineup()
   .then(res => {
-    console.log(res);
-  })
-  .catch(err => {
-    console.log(err);
-  })
+    if(res.game && res.teams){
+      // Sorting out/formatting variables
+      const gameTime = moment(res.game.startTime).format("HH");
+      const dayNight = () => {
+        const hour = parseInt(gameTime);
+        if(hour >= 17) {
+          return "N"
+        } else {
+          return "D"
+        }
+      };
+      const gamedate = res.game.startTime;
+      const dayofweek = moment(gamedate).format('ddd');
 
-  // getLineup(date, away, home)
-  // .then(res => {
-  //   if(res.data){
-
-  //     const gameData = {
-  //       date: date,
-  //       game_num: "1",
-  //       day_of_week: moment().day(date),
-  //       visiting_team: res.data.game.awayTeam.abbreviation,
-  //       visiting_team_league: "",
-  //       visiting_game_num: "",
-  //       home_team: 1,
-  //       home_team_league: "",
-  //       home_team_game_num: "",
-  //       day_night: 1,
-  //       park_id: 1,
-  //       visiting_manager_name: "",
-  //       home_manager_name: "",
-  //       visiting_starting_pitcher_name: 2,
-  //       home_starting_pitcher_name: 2,
-  //       visiting_1_name: 2,
-  //       visiting_1_pos: 2,
-  //       visiting_2_name: 2,
-  //       visiting_2_pos: 2,
-  //       visiting_3_name: 2,
-  //       visiting_3_pos: 2,
-  //       visiting_4_name: 2,
-  //       visiting_4_pos: 2,
-  //       visiting_5_name: 2,
-  //       visiting_5_pos: 2,
-  //       visiting_6_name: 2,
-  //       visiting_6_pos: 2,
-  //       visiting_7_name: 2,
-  //       visiting_7_pos: 2,
-  //       visiting_8_name: 2,
-  //       visiting_8_pos: 2,
-  //       visiting_9_name: 2,
-  //       visiting_9_pos: 2,
-  //       home_1_name: 2,
-  //       home_1_pos: 2,
-  //       home_2_name: 2,
-  //       home_2_pos: 2,
-  //       home_3_name: 2,
-  //       home_3_pos: 2,
-  //       home_4_name: 2,
-  //       home_4_pos: 2,
-  //       home_5_name: 2,
-  //       home_5_pos: 2,
-  //       home_6_name: 2,
-  //       home_6_pos: 2,
-  //       home_7_name: 2,
-  //       home_7_pos: 2,
-  //       home_8_name: 2,
-  //       home_8_pos: 2,
-  //       home_9_name: 2,
-  //       home_9_pos: 2
-  //     }
-  //   } else {
-  //     console.log(res);
-  //   }
-  // }
-  // )
+      console.log(res, gameTime);
+      const gameData = {
+        date: date,
+        game_num: "3",
+        day_of_week: dayofweek,
+        visiting_team: res.game.awayTeam.abbreviation,
+        visiting_team_league: `${res.teams[0].data[0].league} ${res.teams[0].data[0].division}`,
+        visiting_game_num: "1",
+        home_team: res.game.homeTeam.abbreviation,
+        home_team_league: `${res.teams[1].data[0].league} ${res.teams[1].data[0].division}`,
+        home_team_game_num: "1",
+        day_night: dayNight(),
+        park_id: res.game.venue.id,
+        visiting_manager_name: "",
+        home_manager_name: "",
+        visiting_starting_pitcher_name: 2,
+        home_starting_pitcher_name: 2,
+        visiting_1_name: 2,
+        visiting_1_pos: 2,
+        visiting_2_name: 2,
+        visiting_2_pos: 2,
+        visiting_3_name: 2,
+        visiting_3_pos: 2,
+        visiting_4_name: 2,
+        visiting_4_pos: 2,
+        visiting_5_name: 2,
+        visiting_5_pos: 2,
+        visiting_6_name: 2,
+        visiting_6_pos: 2,
+        visiting_7_name: 2,
+        visiting_7_pos: 2,
+        visiting_8_name: 2,
+        visiting_8_pos: 2,
+        visiting_9_name: 2,
+        visiting_9_pos: 2,
+        home_1_name: 2,
+        home_1_pos: 2,
+        home_2_name: 2,
+        home_2_pos: 2,
+        home_3_name: 2,
+        home_3_pos: 2,
+        home_4_name: 2,
+        home_4_pos: 2,
+        home_5_name: 2,
+        home_5_pos: 2,
+        home_6_name: 2,
+        home_6_pos: 2,
+        home_7_name: 2,
+        home_7_pos: 2,
+        home_8_name: 2,
+        home_8_pos: 2,
+        home_9_name: 2,
+        home_9_pos: 2
+      }
+      console.log(gameData);
+    } else {
+      console.log(res);
+    }
+  }
+  )
 }
