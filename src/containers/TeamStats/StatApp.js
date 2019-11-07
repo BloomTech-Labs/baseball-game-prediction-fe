a3e1ff1c825da0698e9a350722fbe70a86006349
duckets615 <chris.reilly024@gmail.com>
2019-11-06 23:16:36 -0500

    init file setup

---
+++ src/containers/TeamStats/StatApp.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class StatApp extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                main
+            </div>
+        )
+    }
+}
+
+export default StatApp;
